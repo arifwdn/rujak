206cce7569bb1e38e293e36aabab603cbe3da96f
arifwdn <muhammadarifwardana01@gmail.com>
2025-04-12 10:51:07 +0800

    init project

---
+++ application/views/login.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+    <center>
+        <div class="card">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </center>
+    <form action="" class="row">
+        <div class="col-auto">
+            <label for="username" class="visually-hidden">Username</label>
+            <input type="text" class="form-control" id="username" value="" placeholder="type username here...">
+        </div>
+        <div class="col-auto">
+            <label for="password" class="visually-hidden">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="type password here">
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Login</button>
+        </div>
+    </form>
+</div>
---
+++ application/views/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+    <title>Rujak - <?= $title ?></title>
+</head>
+
+<body>
+    <?= $content; ?>
+    <script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+</body>
+
+</html>
