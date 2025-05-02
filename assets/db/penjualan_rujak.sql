7f71dc2020be867f9160a79041be599daf02d069
arifwdn <muhammadarifwardana01@gmail.com>
2025-05-02 14:35:02 +0800

    edit: perubahan file sql

--- assets/db/penjualan_rujak.sql
+++ assets/db/penjualan_rujak.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 21, 2025 at 03:03 AM
+-- Generation Time: May 02, 2025 at 06:33 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,9 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga`, `satuan`, `stok`) VALUES
-(1, 'Rujak', 4000, 'pcs', 3);
+(1, 'Rujak uk.kecil', 4000, 'pcs', 0),
+(6, 'Rujak uk.sedang', 8000, 'pcs', 3),
+(7, 'Rujak uk.besar', 12000, 'pcs', 3);
 
 -- --------------------------------------------------------
 
@@ -55,6 +57,15 @@ CREATE TABLE `customer` (
   `lokasi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id_customer`, `nama`, `no_hp`, `lokasi`) VALUES
+(3, 'Paman Zilong', '6285284931233', 'Samping Langgar\r\n'),
+(4, 'Acil Odette', '6282158292042', 'Asam-Asam\r\n'),
+(5, 'Acil Funny', '6282178993748', 'Jl. Majakeling\r\n');
+
 -- --------------------------------------------------------
 
 --
@@ -62,7 +73,7 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `detail_transaksi` (
-  `ip_detail_transaksi` int NOT NULL,
+  `id_detail_transaksi` int NOT NULL,
   `id_transaksi` int NOT NULL,
   `id_barang` int NOT NULL,
   `quantity` int NOT NULL,
@@ -70,6 +81,22 @@ CREATE TABLE `detail_transaksi` (
   `sisa` int NOT NULL,
   `total` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail_transaksi`, `id_transaksi`, `id_barang`, `quantity`, `jumlah_terjual`, `sisa`, `total`) VALUES
+(7, 4, 1, 8, 0, 0, 32000),
+(12, 7, 1, 4, 1, 3, 4000),
+(13, 7, 6, 3, 1, 2, 8000),
+(14, 7, 7, 5, 1, 4, 12000),
+(15, 8, 1, 2, 1, 1, 4000),
+(16, 8, 7, 3, 1, 2, 12000),
+(17, 9, 1, 4, 0, 0, 16000),
+(18, 9, 6, 4, 0, 0, 32000),
+(19, 9, 7, 3, 0, 0, 36000),
+(20, 10, 6, 3, 0, 0, 24000);
 
 -- --------------------------------------------------------
 
@@ -82,9 +109,20 @@ CREATE TABLE `transaksi` (
   `id_customer` int NOT NULL,
   `tanggal` date NOT NULL,
   `total_pendapatan` int NOT NULL,
-  `sudah_diambil` timestamp NOT NULL,
+  `sudah_diambil` timestamp NULL DEFAULT NULL,
   `pengambil` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_customer`, `tanggal`, `total_pendapatan`, `sudah_diambil`, `pengambil`) VALUES
+(4, 3, '2025-04-28', 32000, '2025-04-28 08:51:19', 'aing'),
+(7, 4, '2025-04-30', 24000, '2025-05-01 19:42:12', 'Aing'),
+(8, 3, '2025-05-02', 16000, '2025-05-01 19:42:33', 'Sori'),
+(9, 4, '2025-05-02', 84000, NULL, ''),
+(10, 3, '2025-05-01', 24000, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -103,7 +141,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
-(1, 'admin', '$2y$10$GFEcvrS8hj6aXAQMgmnAkuYQsvPXBy6HE2Zp2Q4Ef8Oc.jHYevYD2');
+(1, 'admin', '$2y$10$GFEcvrS8hj6aXAQMgmnAkuYQsvPXBy6HE2Zp2Q4Ef8Oc.jHYevYD2'),
+(2, 'afif', '$2y$10$iI7db2f0nl..iwpNrftUCerKVldMHch6gZ.742tZBbaIrJvxQ24zi');
 
 --
 -- Indexes for dumped tables
@@ -125,7 +164,7 @@ ALTER TABLE `customer`
 -- Indexes for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  ADD PRIMARY KEY (`ip_detail_transaksi`),
+  ADD PRIMARY KEY (`id_detail_transaksi`),
   ADD KEY `fk_transaksi` (`id_transaksi`),
   ADD KEY `fk_barang` (`id_barang`);
 
@@ -151,31 +190,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id_customer` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_customer` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `ip_detail_transaksi` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -185,14 +224,14 @@ ALTER TABLE `user`
 -- Constraints for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`);
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
